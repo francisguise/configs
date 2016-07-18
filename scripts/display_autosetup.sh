17496519e82fc1134c2da3485f7ab95fac833499
Francis Guise <francis.guise@openbet.com>
2016-07-18 17:05:01 +0100

    updates

--- scripts/display_autosetup.sh
+++ scripts/display_autosetup.sh
@@ -1,27 +1,25 @@
-
+#!/bin/bash
 [ $# -ne 0 ] && [ $1 == "quiet" ] && quiet=1 || quiet=0
 
 connected_displays=$( xrandr | grep ' connected ' | awk '{print $1}' )
+for display in ${connected_displays} ; do
+	eval $display=1
+done
 
-#iter=0
-#for display in ${connected_displays} ; do
-#	eval export DISP${iter}=$display
-#	iter+=1
-#done
-
-if [ ${#connected_displays[@]} -gt 1 ] ; then
+echo "> $HDMI1 , $VGA1"
+if [[ "$HDMI1" == 1 && "$VGA1" == 1 ]] ; then
 	#Dual monitor setup
-	[ $quiet ] || xrandr --output HDMI1 --auto --left-of VGA1 --output VGA1 --auto
-	export DISP1=HDMI1
-	export DISP2=VGA1
+	#xrandr --output HDMI1 --auto --left-of VGA1 --output VGA1 --auto
+	xrandr  --output  LVDS1 --off --output HDMI1 --auto --left-of VGA1 --output VGA1 --auto
 else
 	#Just laptop
-	[ $quiet ] || xrandr --output ${connected_displays[0]} --auto
-	export DISP1=LVDS1
-	export DISP2=LVDS1
+	xrandr --output LVDS1 --auto --output VGA1 --off
 fi
-
 # reset background
-[ $quiet ] || sh ~/.fehbg
-[ $quiet ] ||  i3-nagbar -t warning -m "You reconfigured the connected_displays: $connected_displays!"
-
+sh ~/.fehbg
+i3-nagbar -t warning -m "You reconfigured the connected_displays: $connected_displays!"
+#notify-send --icon=gtk-info Test "Displays $connected_displays reconfigured"
+#_vga=$( xrandr  | sed -n 's/^\(VGA.\).*/\1/pg')
+#alias laptop='xrandr --output LVDS1 --auto --output $_vga --off'
+#alias monitor='xrandr --output LVDS1 --off --output $_vga --auto'
+#alias dual='xrandr --output LVDS1 --auto --left-of $_vga --output $_vga --auto'
